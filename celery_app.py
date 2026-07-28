a554621d92de2793540305e34a63159071505e95
editsbyrafayyy <rafayatherk@gmail.com>
2026-07-28 23:04:38 +0500

    fix(celery): added tasks file as a parameter to setup

--- celery_app.py
+++ celery_app.py
@@ -2,7 +2,8 @@ from celery import Celery
 
 celery_app = Celery(
 	"subscriptions_tasks", # name for the celery app, will be mainly used internally
-	broker="redis://redis:6379/0", # broker here means where celery will send/reads tasks from, which in this case is redis
-	backend="redis://redis:6379/0", # backend is where celery will be storing the results for the tasks at (not entirly needed here but standard practice to include)
+	broker="redis://localhost:6379/0", # broker here means where celery will send/reads tasks from, which in this case is redis
+	backend="redis://localhost:6379/0", # backend is where celery will be storing the results for the tasks at (not entirly needed here but standard practice to include)
 	# the /0 signals database 0 as redis organizes data into databases 0-15
+	include=["tasks"],
 	)
---
+++ test.py
@@ -0,0 +1,4 @@
+from tasks import check_upcoming_subs
+
+check_upcoming_subs.delay()
+
